d8bf54619e809c4fc02c5cf699c6312118265c38
davitasryant <davit.asryan.y@tumo.org>
2022-11-08 18:30:19 +0400

    Jarangakanutyun Final

--- class.mulboost.js
+++ class.mulboost.js
@@ -3,33 +3,6 @@ class Mulboost extends LivingCreature{
         super(x,y);
         this.directions = [];
     }
-    // getNewCoordinates() {
-    //     this.directions = [
-    //         [this.x - 1, this.y - 1],
-    //         [this.x, this.y - 1],
-    //         [this.x + 1, this.y - 1],
-    //         [this.x - 1, this.y],
-    //         [this.x + 1, this.y],
-    //         [this.x - 1, this.y + 1],
-    //         [this.x, this.y + 1],
-    //         [this.x + 1, this.y + 1]
-    //     ];
-    // }
-
-    // search(char) {
-    //     this.getNewCoordinates();
-    //     let found = [];
-    //     for (let i in this.directions) {
-    //         let x = this.directions[i][0];
-    //         let y = this.directions[i][1];
-    //         if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-    //             if (matrix[y][x] == char) {
-    //                 found.push(this.directions[i])
-    //             }
-    //         }
-    //     }
-    //     return found;
-    // }
 
     search(ch) {
         this.getNewCoordinates()
